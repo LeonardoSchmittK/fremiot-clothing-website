8604b90174d0102554f76e28b178a9c769af0843
leoschmittk <leonardoschmittkr@gmail.com>
2023-12-03 22:15:50 -0300

    first

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["index.html", "index.css"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        calca: "url('./image/calca.png')",
+        cropped: "url('./image/cropped.png')",
+      },
+    },
+  },
+  plugins: [],
+};
